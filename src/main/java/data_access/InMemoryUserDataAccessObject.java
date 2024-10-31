2be4e95dc9f166b09a5daee4bfef616da92baa8d
Rolland <hexucheng2004@gmail.com>
2024-10-31 16:05:11 -0400

    10.31 Rolland He add three java files from lab-5 under data_access

---
+++ src/main/java/data_access/InMemoryUserDataAccessObject.java
@@ -0,0 +1,55 @@
+package data_access;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import entity.User;
+import use_case.change_password.ChangePasswordUserDataAccessInterface;
+import use_case.login.LoginUserDataAccessInterface;
+import use_case.logout.LogoutUserDataAccessInterface;
+import use_case.signup.SignupUserDataAccessInterface;
+
+/**
+ * In-memory implementation of the DAO for storing user data. This implementation does
+ * NOT persist data between runs of the program.
+ */
+public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterface,
+        LoginUserDataAccessInterface,
+        ChangePasswordUserDataAccessInterface,
+        LogoutUserDataAccessInterface {
+
+    private final Map<String, User> users = new HashMap<>();
+
+    private String currentUsername;
+
+    @Override
+    public boolean existsByName(String identifier) {
+        return users.containsKey(identifier);
+    }
+
+    @Override
+    public void save(User user) {
+        users.put(user.getName(), user);
+    }
+
+    @Override
+    public User get(String username) {
+        return users.get(username);
+    }
+
+    @Override
+    public void changePassword(User user) {
+        // Replace the old entry with the new password
+        users.put(user.getName(), user);
+    }
+
+    @Override
+    public void setCurrentUsername(String name) {
+        this.currentUsername = name;
+    }
+
+    @Override
+    public String getCurrentUsername() {
+        return this.currentUsername;
+    }
+}
