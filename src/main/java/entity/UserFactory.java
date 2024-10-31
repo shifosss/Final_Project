ecc573eaa14938c02487ab1bc30ad3408fe74fa0
Rolland <hexucheng2004@gmail.com>
2024-10-31 16:09:16 -0400

    10.31 Rolland He add four java files from lab-5 under entity

---
+++ src/main/java/entity/UserFactory.java
@@ -0,0 +1,15 @@
+package entity;
+
+/**
+ * Factory for creating users.
+ */
+public interface UserFactory {
+    /**
+     * Creates a new User.
+     * @param name the name of the new user
+     * @param password the password of the new user
+     * @return the new user
+     */
+    User create(String name, String password);
+
+}
