0d151c8ac087801df2c9c0bf522198b94eaef7e9
fanke <fankeqin@outlook.com>
2024-11-15 14:36:49 -0500

    makeApirequest test

---
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -0,0 +1,30 @@
+package use_case.login.login;
+
+import entity.User;
+
+/**
+ * DAO for the Login Use Case.
+ */
+public interface LoginUserDataAccessInterface {
+
+    /**
+     * Checks if the given username exists.
+     * @param username the username to look for
+     * @return true if a user with the given username exists; false otherwise
+     */
+    boolean existsByName(String username);
+
+    /**
+     * Saves the user.
+     * @param user the user to save
+     */
+    void save(User user);
+
+    /**
+     * Returns the user with the given username.
+     * @param username the username to look up
+     * @return the user with the given username
+     */
+    User get(String username);
+
+}
