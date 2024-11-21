c251dd41b8bb4b75de2f458b3030c83bbcc8602e
sHin7823 <migz.galang@mail.utoronto.ca>
2024-11-21 11:43:53 -0500

    Filler codes for random recipe usecase

---
+++ src/main/java/use_case/random_recipes/RandomRecipeInputBoundary.java
@@ -0,0 +1,12 @@
+package use_case.random_recipes;
+
+/**
+ * Input Boundary for the random recipe.
+ */
+public interface RandomRecipeInputBoundary {
+
+    /**
+     * Executes the random recipe usecase.
+     */
+    void execute();
+}
