f49983a93d755a09503d7235ba427e73edddcd10
sHin7823 <migz.galang@mail.utoronto.ca>
2024-11-21 15:14:42 -0500

    Updated custom recipe use case

--- src/main/java/use_case/create_recipe/CustomRecipeDataAccessInterface.java
+++ src/main/java/use_case/create_recipe/CustomRecipeDataAccessInterface.java
@@ -1,8 +1,13 @@
 package use_case.create_recipe;
 
+import entities.recipe.Recipe;
+
 /**
  * DAO for the creation of custom recipe usecase.
  */
 public interface CustomRecipeDataAccessInterface {
-    void
+    void createCustomRecipe(Recipe recipe);
+
+    void removeCustomRecipe(int id);
+
 }
