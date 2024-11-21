3b9335d6fa40e349dcbc825301d9d092b1d52f8b
sHin7823 <migz.galang@mail.utoronto.ca>
2024-11-21 10:56:47 -0500

    Implemented DAO for bookmark usecase.

--- src/main/java/data_access/UserDataAccessObject.java
+++ src/main/java/data_access/UserDataAccessObject.java
@@ -8,9 +8,11 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 
 import entities.user.User;
+import entities.user.factory.CommonUserFactory;
 import entities.user.factory.UserFactory;
 import exceptions.UserNotFound;
 import org.bson.Document;
+import use_case.bookmark_recipe.BookmarkRecipeDataAccessInterface;
 import use_case.login.LoginDataAccessInterface;
 import use_case.signup.SignupDataAccessInterface;
 
@@ -19,12 +21,19 @@ import java.util.List;
 /**
  * The Data Access Object for users.
  */
-public class UserDataAccessObject implements LoginDataAccessInterface, SignupDataAccessInterface {
+public class UserDataAccessObject implements LoginDataAccessInterface,
+        SignupDataAccessInterface, BookmarkRecipeDataAccessInterface {
     private static final String ACCESS_USERNAME = "appUser";
     private static final String ACCESS_PASSWORD = "myPassword123";
     private static final String CONNECTION_URL = String.format("mongodb+srv://%s:%s@cluster0.dvuik.mongodb.net/",
             ACCESS_USERNAME, ACCESS_PASSWORD)
             + "?retryWrites=true&w=majority&appName=Cluster0";
+
+    private static final String USERNAME_ATTRIBUTE = "username";
+    private static final String PASSWORD_ATTRIBUTE = "password";
+    private static final String BOOKMARKS_ATTRIBUTE = "bookmarkedRecipeIds";
+    private static final String CREATED_RECIPES_ATTRIBUTE = "createdRecipes";
+    private static final String INGREDIENTS_ATTRIBUTE = "ingredientsToAvoid";
 
     private static final String RECIPE_DATABASE_NAME = "recipe_app_db";
     private static final String USERS_COLLECTION_NAME = "users";
@@ -135,5 +144,31 @@ public class UserDataAccessObject implements LoginDataAccessInterface, SignupDat
 
     private boolean validatePassword(String password) {
         return password.length() >= 6;
+    }
+
+    @Override
+    public boolean isBookmarked(String username, int recipeId) {
+        if (!existsByName(username)) {
+            throw new UserNotFound(username);
+        }
+        try (MongoClient mongoClient = MongoClients.create(CONNECTION_URL)) {
+            final MongoDatabase database = mongoClient.getDatabase(RECIPE_DATABASE_NAME);
+            final MongoCollection<Document> usersCollection = database.getCollection(USERS_COLLECTION_NAME);
+
+            final Document foundUser = usersCollection.find(Filters.and(
+                    Filters.eq("username", username), Filters.eq("bookmarkedRecipeIds", 11007))
+            ).first();
+            return foundUser != null;
+        }
+    }
+
+    @Override
+    public void bookmarkRecipe(String username, int recipeId) {
+    }
+
+    public static void main(String[] args) {
+        final UserFactory userFactory1 = new CommonUserFactory();
+        final UserDataAccessObject userDataAccessObject = new UserDataAccessObject(userFactory1);
+        System.out.println(userDataAccessObject.isBookmarked("shin", 11007));
     }
 }
