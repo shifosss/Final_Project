2110fd69f6f325f873f2e3b5ab6301594845676c
sHin7823 <migz.galang@mail.utoronto.ca>
2024-11-25 19:27:23 -0500

    Fixed some bugs

--- src/main/java/interface_adapter/preference/PreferenceState.java
+++ src/main/java/interface_adapter/preference/PreferenceState.java
@@ -1,7 +1,36 @@
 package interface_adapter.preference;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * State for the preference view.
  */
 public class PreferenceState {
+    private List<String> ingredientList = new ArrayList<>();
+    private String username = "";
+
+    public PreferenceState(PreferenceState copy) {
+        this.ingredientList = copy.ingredientList;
+        this.username = copy.username;
+    }
+
+    public PreferenceState() {
+    }
+
+    public List<String> getIngredientList() {
+        return ingredientList;
+    }
+
+    public void setIngredientList(List<String> ingredientList) {
+        this.ingredientList = ingredientList;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
